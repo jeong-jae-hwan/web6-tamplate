113f22e4c41506452943240b0b386ccb5fa2a0f8
jeongjaehwan <jeahwani03@gmail.com>
2022-09-12 19:27:29 +0900

    '09.12보강'

--- pages/_app.js
+++ pages/_app.js
@@ -2,18 +2,19 @@ import 'styles/@globals.css'
 import { store } from '/@redux/app/store'
 import { Provider } from 'react-redux'
 import Layout from 'public/Layout'
+import { useRouter } from 'next/router'
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter()
-  useEffect(() => {
-    const handleRouteChange = url => {
-      gtag.pageview(url)
-    }
-    router.events.on('routeChangeComplete', handleRouteChange)
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
+  // useEffect(() => {
+  //   const handleRouteChange = url => {
+  //     gtag.pageview(url)
+  //   }
+  //   router.events.on('routeChangeComplete', handleRouteChange)
+  //   return () => {
+  //     router.events.off('routeChangeComplete', handleRouteChange)
+  //   }
+  // }, [router.events])
 
   return (
     <>
