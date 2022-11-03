2c3f32fe852352132da1847b744ba7962caa0960
jeongjaehwan <jeahwani03@gmail.com>
2022-11-03 13:21:22 +0900

    '보강'

--- custom/modals/AlartSnackbar.js
+++ custom/modals/AlartSnackbar.js
@@ -1,47 +1,51 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react'
 
 //style
-import { SnackbarWrap, Snackbar } from 'scss/modal-styled/Snackbar';
-import { IconTab } from 'scss/tab-styled/IconTab';
+import { ThemeProvider } from '@emotion/react'
+import { SnackbarWrap, Snackbar } from 'scss/modal-styled/Snackbar'
+import { IconTab } from 'scss/tab-styled/IconTab'
 
 //svg
-import TabIcon from 'public/icons/x-tab-icon.svg';
+import TabIcon from 'public/icons/x-tab-icon.svg'
 
 //
 export default function AlartSnackbar(props) {
-  const { isActive, isCancel, children } = props;
+  const { isActive, isCancel, children } = props
+  const theme = {
+    mobile: '600px',
+  }
 
   // 모달 고정 및 외부 클릭감지
-  const ref = useRef();
-  const clickModalOutside = (event) => {
+  const ref = useRef()
+  const clickModalOutside = event => {
     if (isActive && !ref.current?.contains(event.target)) {
-      isCancel();
+      isCancel()
     }
-  };
+  }
 
   useEffect(() => {
-    document.addEventListener('mousedown', clickModalOutside);
+    document.addEventListener('mousedown', clickModalOutside)
     return () => {
-      document.removeEventListener('mousedown', clickModalOutside);
-    };
-  }, [isActive]);
+      document.removeEventListener('mousedown', clickModalOutside)
+    }
+  }, [isActive])
 
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <SnackbarWrap isActive={isActive} ref={ref}>
         <Snackbar>
           <IconTab
-            position='absolute'
-            top='10px'
-            right='10px'
-            width='22px'
+            position="absolute"
+            top="10px"
+            right="10px"
+            width="22px"
             onClick={isCancel}
           >
-            <TabIcon fill='#ddd' width='22px' height='22px' />
+            <TabIcon fill="#ddd" width="22px" height="22px" />
           </IconTab>
           {children}
         </Snackbar>
       </SnackbarWrap>
-    </>
-  );
+    </ThemeProvider>
+  )
 }
