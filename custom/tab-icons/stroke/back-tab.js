930f7f5697219c1a2bb6fc4a78cd7005d460007a
jeongjaehwan <jeahwani03@gmail.com>
2023-01-16 13:30:08 +0900

    커스텀업로드

--- custom/tab-icons/stroke/back-tab.js
+++ custom/tab-icons/stroke/back-tab.js
@@ -21,9 +21,11 @@ export default function BackTab(props) {
     >
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        width={size ? size : '18px'}
-        height={size ? size : '18px'}
         viewBox="0 0 22 22"
+        css={css`
+          width: 100%;
+          height: 100%;
+        `}
       >
         <g id="back-icon" transform="translate(-216 -86)">
           <rect
